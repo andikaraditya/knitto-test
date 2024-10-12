74cf381a7e6fc0a2e2097292550df96fa7dc7412
andikaraditya <andikaraditya1@gmail.com>
2024-10-12 08:47:47 +0700

    init middleware

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express, { Request, Response } from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import morgan from "morgan";
 
 (async () => {
   const app = express();
   const port = process.env.PORT || 3000;
+
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ limit: "250mb", extended: true }));
+  app.use(bodyParser.text());
+  app.use(bodyParser.json({ limit: "250mb" }));
+  app.use(morgan("dev"));
 
   app.get("/", (req: Request, res: Response) => {
     res.send("Hello, World");
